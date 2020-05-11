6977d52fdf51dcc197e56c8042c5799fb01af22f
phatle281200 <63404247+phatle281200@users.noreply.github.com>
2020-05-11 19:16:17 +0200

    Update_Staff (Phat)

--- G6/DAO/StaffDAO.cs
+++ G6/DAO/StaffDAO.cs
@@ -3,10 +3,21 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Configuration;
+using System.Data.SqlClient;
+using Model;
 
 namespace DAO
 {
     public class StaffDAO
     {
+        private SqlConnection dbConnection;
+        public StaffDAO()
+        {
+            string connString = ConfigurationManager
+                                    .ConnectionStrings["DBConnectionString"]
+                                    .ConnectionString;
+            dbConnection = new SqlConnection(connString);
+        }
     }
 }
--- G6/Logic/StaffService.cs
+++ G6/Logic/StaffService.cs
@@ -3,10 +3,14 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Model;
+using DAO;
 
 namespace Logic
 {
     public class StaffService
     {
+        private StaffDAO staffDAO = new StaffDAO();
+
     }
 }
