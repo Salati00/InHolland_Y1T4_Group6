8ca4a1e4e6ab535f80f630228a66cd066fb563d1
Giorgio <salati00.giorgio@gmail.com>
2020-05-12 16:20:06 +0200

    Ordering - Main proper query fixed

--- G6/DAO/OrderingDAO.cs
+++ G6/DAO/OrderingDAO.cs
@@ -24,10 +24,10 @@ namespace DAO
         }
         public List<StringValueWrapper> Db_Get_Item_Names(string TypeName)
         {
-            //string query = "SELECT Name from [Menu_Items] WHERE Item_Type_ID in (Select Item_Type_ID FROM Item_Types where Name like '@nome')";
-            string query = "SELECT * from [Menu_Items]";
-            SqlParameter[] sqlParameters = new SqlParameter[0];
-            //sqlParameters[0] = new SqlParameter("@nome", TypeName);
+            string query = "SELECT Name from [Menu_Items] WHERE Item_Type_ID in (Select Item_Type_ID FROM Item_Types where Name like @nome)";
+            //string query = "SELECT * from [Menu_Items]";
+            SqlParameter[] sqlParameters = new SqlParameter[1];
+            sqlParameters[0] = new SqlParameter("@nome", TypeName);
             return ReadNames(ExecuteSelectQuery(query, sqlParameters));
         }
 
