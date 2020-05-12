0121403965430fb43c8d9bf01c0f5497513008d8
momo-98 <54842875+momo-98@users.noreply.github.com>
2020-05-12 14:55:28 +0200

    Momo stuff

---
+++ G6/DAO/kichenBarDAO.cs
@@ -0,0 +1,44 @@
+﻿using Model;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+
+
+namespace DAO
+{
+    class kichenBarDAO : Base
+    {
+
+        public List<Order> Db_Get_Orders()
+        {
+            string query = "";
+            SqlParameter[] sqlParameters = new SqlParameter[0];
+            return ReadTables(ExecuteSelectQuery(query, sqlParameters));
+        }
+        private List<Order> ReadTables(DataTable dataTable)
+        {
+            List<Order> orders = new List<Order>();
+
+            foreach (DataRow dr in dataTable.Rows)
+            {
+                Order order = new Order()
+                {
+                    Order_Item_Id = (int)dr[""],
+                    Time = (DateTime)dr[""],
+                    Quantity = (int)dr[""],
+                    Comment = (string)dr[""],
+                    ItemName = (string)dr[""],
+                    ItemType = (string)dr[""],
+                    state = (string)dr[""]
+
+                };
+                orders.Add(order);
+            }
+            return orders;
+        }
+    }
+}
---
+++ G6/Model/Order.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Model
+{
+    public class Order
+    {
+        //order items
+        public int Order_Item_Id { get; set; }
+        public DateTime Time { get; set; }
+        public int Quantity { get; set; }
+        public string Comment { get; set; }
+
+        //menu items
+        public string ItemName { get; set; }
+        public string ItemType { get; set; }
+
+        //state
+        public string state { get; set; }
+
+
+    }
+}
