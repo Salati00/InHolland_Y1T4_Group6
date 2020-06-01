bbae257f6c25b4be0f93b238d5531232af0b32da
momo-98 <54842875+momo-98@users.noreply.github.com>
2020-06-01 06:06:51 +0200

    design

--- G6/Start/KitchenBar.Designer.cs
+++ G6/Start/KitchenBar.Designer.cs
@@ -29,102 +29,136 @@
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
+            System.Windows.Forms.ListViewItem listViewItem71 = new System.Windows.Forms.ListViewItem("");
+            System.Windows.Forms.ListViewItem listViewItem72 = new System.Windows.Forms.ListViewItem("");
+            System.Windows.Forms.ListViewItem listViewItem73 = new System.Windows.Forms.ListViewItem("");
+            System.Windows.Forms.ListViewItem listViewItem74 = new System.Windows.Forms.ListViewItem("");
+            System.Windows.Forms.ListViewItem listViewItem75 = new System.Windows.Forms.ListViewItem("");
+            System.Windows.Forms.ListViewItem listViewItem76 = new System.Windows.Forms.ListViewItem("");
+            System.Windows.Forms.ListViewItem listViewItem77 = new System.Windows.Forms.ListViewItem("");
+            System.Windows.Forms.ListViewItem listViewItem78 = new System.Windows.Forms.ListViewItem("");
+            System.Windows.Forms.ListViewItem listViewItem79 = new System.Windows.Forms.ListViewItem("");
+            System.Windows.Forms.ListViewItem listViewItem80 = new System.Windows.Forms.ListViewItem(new string[] {
+            "click",
+            "click"}, -1);
             this.PanelOrders = new System.Windows.Forms.FlowLayoutPanel();
-            this.recalled = new System.Windows.Forms.Button();
             this.recallpanel = new System.Windows.Forms.Panel();
-            this.listBox1 = new System.Windows.Forms.ListBox();
-            this.recall = new System.Windows.Forms.Button();
-            this.back = new System.Windows.Forms.Button();
-            this.timee = new System.Windows.Forms.Label();
+            this.listViewrecall = new System.Windows.Forms.ListView();
+            this.columnHeader1 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
             this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.timee = new System.Windows.Forms.Label();
+            this.Home = new System.Windows.Forms.Button();
+            this.recall = new System.Windows.Forms.Button();
             this.recallpanel.SuspendLayout();
             this.SuspendLayout();
             // 
             // PanelOrders
             // 
-            this.PanelOrders.Location = new System.Drawing.Point(12, 12);
+            this.PanelOrders.BackColor = System.Drawing.Color.Transparent;
+            this.PanelOrders.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.PanelOrders.Location = new System.Drawing.Point(32, 12);
             this.PanelOrders.Name = "PanelOrders";
-            this.PanelOrders.Size = new System.Drawing.Size(776, 426);
+            this.PanelOrders.Size = new System.Drawing.Size(1958, 936);
             this.PanelOrders.TabIndex = 0;
-            // 
-            // recalled
-            // 
-            this.recalled.Location = new System.Drawing.Point(0, 0);
-            this.recalled.Name = "recalled";
-            this.recalled.Size = new System.Drawing.Size(335, 176);
-            this.recalled.TabIndex = 0;
-            this.recalled.TextAlign = System.Drawing.ContentAlignment.TopLeft;
-            this.recalled.UseVisualStyleBackColor = true;
-            this.recalled.Click += new System.EventHandler(this.recalled_Click);
+            this.PanelOrders.Paint += new System.Windows.Forms.PaintEventHandler(this.PanelOrders_Paint);
             // 
             // recallpanel
             // 
-            this.recallpanel.Controls.Add(this.listBox1);
-            this.recallpanel.Controls.Add(this.recalled);
-            this.recallpanel.Location = new System.Drawing.Point(213, 122);
+            this.recallpanel.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.recallpanel.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.recallpanel.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.recallpanel.Controls.Add(this.listViewrecall);
+            this.recallpanel.ForeColor = System.Drawing.Color.White;
+            this.recallpanel.Location = new System.Drawing.Point(484, 229);
             this.recallpanel.Name = "recallpanel";
-            this.recallpanel.Size = new System.Drawing.Size(335, 176);
+            this.recallpanel.Size = new System.Drawing.Size(926, 462);
             this.recallpanel.TabIndex = 1;
             // 
-            // listBox1
+            // listViewrecall
             // 
-            this.listBox1.FormattingEnabled = true;
-            this.listBox1.ItemHeight = 20;
-            this.listBox1.Items.AddRange(new object[] {
-            " cmd,ackl\\",
-            "scdslmcdms",
-            "dscdsmc"});
-            this.listBox1.Location = new System.Drawing.Point(8, 8);
-            this.listBox1.Name = "listBox1";
-            this.listBox1.Size = new System.Drawing.Size(298, 144);
-            this.listBox1.TabIndex = 1;
-            this.listBox1.SelectedIndexChanged += new System.EventHandler(this.listBox1_SelectedIndexChanged);
-            // 
-            // recall
-            // 
-            this.recall.Location = new System.Drawing.Point(323, 444);
-            this.recall.Name = "recall";
-            this.recall.Size = new System.Drawing.Size(120, 36);
-            this.recall.TabIndex = 2;
-            this.recall.Text = "recall";
-            this.recall.UseVisualStyleBackColor = true;
-            this.recall.Click += new System.EventHandler(this.recall_Click);
-            // 
-            // back
-            // 
-            this.back.Location = new System.Drawing.Point(12, 444);
-            this.back.Name = "back";
-            this.back.Size = new System.Drawing.Size(116, 36);
-            this.back.TabIndex = 3;
-            this.back.Text = "back";
-            this.back.UseVisualStyleBackColor = true;
-            this.back.Click += new System.EventHandler(this.back_Click);
-            // 
-            // timee
-            // 
-            this.timee.AutoSize = true;
-            this.timee.Location = new System.Drawing.Point(675, 452);
-            this.timee.Name = "timee";
-            this.timee.Size = new System.Drawing.Size(39, 20);
-            this.timee.TabIndex = 4;
-            this.timee.Text = "time";
+            this.listViewrecall.CausesValidation = false;
+            this.listViewrecall.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.columnHeader1});
+            this.listViewrecall.HideSelection = false;
+            this.listViewrecall.Items.AddRange(new System.Windows.Forms.ListViewItem[] {
+            listViewItem71,
+            listViewItem72,
+            listViewItem73,
+            listViewItem74,
+            listViewItem75,
+            listViewItem76,
+            listViewItem77,
+            listViewItem78,
+            listViewItem79,
+            listViewItem80});
+            this.listViewrecall.Location = new System.Drawing.Point(-2, -2);
+            this.listViewrecall.Name = "listViewrecall";
+            this.listViewrecall.Size = new System.Drawing.Size(921, 457);
+            this.listViewrecall.TabIndex = 0;
+            this.listViewrecall.UseCompatibleStateImageBehavior = false;
+            this.listViewrecall.View = System.Windows.Forms.View.Details;
+            this.listViewrecall.SelectedIndexChanged += new System.EventHandler(this.listViewrecall_SelectedIndexChanged);
+            this.listViewrecall.MouseHover += new System.EventHandler(this.listViewrecall_MouseHover);
             // 
             // timer1
             // 
             this.timer1.Enabled = true;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // timee
+            // 
+            this.timee.AutoSize = true;
+            this.timee.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.timee.Location = new System.Drawing.Point(1684, 942);
+            this.timee.Name = "timee";
+            this.timee.Size = new System.Drawing.Size(81, 37);
+            this.timee.TabIndex = 7;
+            this.timee.Text = "time";
+            // 
+            // Home
+            // 
+            this.Home.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.Home.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.Home.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Home.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.Home.Location = new System.Drawing.Point(32, 926);
+            this.Home.Name = "Home";
+            this.Home.Size = new System.Drawing.Size(186, 69);
+            this.Home.TabIndex = 6;
+            this.Home.Text = "Home";
+            this.Home.UseVisualStyleBackColor = false;
+            this.Home.Click += new System.EventHandler(this.Home_Click);
+            // 
+            // recall
+            // 
+            this.recall.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.recall.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.recall.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.recall.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.recall.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.recall.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.recall.Location = new System.Drawing.Point(859, 926);
+            this.recall.Name = "recall";
+            this.recall.Size = new System.Drawing.Size(186, 69);
+            this.recall.TabIndex = 5;
+            this.recall.Text = "Recall";
+            this.recall.UseVisualStyleBackColor = false;
+            this.recall.Click += new System.EventHandler(this.recall_Click_1);
             // 
             // KitchenBar
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 492);
-            this.Controls.Add(this.timee);
-            this.Controls.Add(this.back);
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(1912, 1024);
             this.Controls.Add(this.recall);
             this.Controls.Add(this.recallpanel);
+            this.Controls.Add(this.Home);
+            this.Controls.Add(this.timee);
             this.Controls.Add(this.PanelOrders);
             this.Name = "KitchenBar";
             this.Text = "Kitchen";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
             this.Load += new System.EventHandler(this.KitchenBar_Load);
             this.recallpanel.ResumeLayout(false);
             this.ResumeLayout(false);
@@ -135,12 +169,12 @@
         #endregion
 
         private System.Windows.Forms.FlowLayoutPanel PanelOrders;
-        private System.Windows.Forms.Button recalled;
         private System.Windows.Forms.Panel recallpanel;
-        private System.Windows.Forms.Button recall;
-        private System.Windows.Forms.Button back;
-        private System.Windows.Forms.Label timee;
         private System.Windows.Forms.Timer timer1;
-        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Label timee;
+        private System.Windows.Forms.Button Home;
+        private System.Windows.Forms.Button recall;
+        private System.Windows.Forms.ListView listViewrecall;
+        private System.Windows.Forms.ColumnHeader columnHeader1;
     }
 }
--- G6/Start/KitchenBar.cs
+++ G6/Start/KitchenBar.cs
@@ -8,6 +8,7 @@ using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows;
 using System.Windows.Forms;
 using System.Windows.Media;
 
@@ -16,90 +17,163 @@ namespace Start
     public partial class KitchenBar : Form
     {
 
-      //  List<Button> buttons;
+        Staff_Types type;
         KitchenBarService service;
 
         public KitchenBar(Staff_Types type)//receive staff instead of type
         {
             InitializeComponent();
-            //  buttons = new List<Button>();
+
+            this.type = type;
             service = new KitchenBarService();
             List<OrderItem> orders = service.GetOrders();
+        }
+         
+
+        void FillInfo()
+        {
             timee.Text = DateTime.Now.ToString("h:mm:ss tt");
             recallpanel.Hide();
 
+            if (type == Staff_Types.Bartender)
+                this.Text = "Bar";
 
             for (int i = 0; i < 4; i++)
         //      foreach(Order order in orders)
             {
-                ListBox button = new ListBox();
+                
+                ListView button = new ListView();
                 button.Height = (PanelOrders.Height / 2) - (PanelOrders.Height / 30);
                 button.Width  = (PanelOrders.Width / 2) - (PanelOrders.Width / 30); ;
                 PanelOrders.Controls.Add(button);
-                //     buttons.Add(button);
+                
                 OrderText(button);
                 button.Click += new EventHandler(this.ClickOrder);
             }
-        }
 
+        }
 
 
         public void ClickOrder(object sender, EventArgs e)
         {
-            ListBox button = (ListBox)sender;
-
+            ListView button = (ListView)sender;            
             PanelOrders.Controls.Remove(button);
-        //    buttons.Remove(button);
+        
 
         }
 
 
         public void NotifyWaiter()
         {
-
+             
         }
 
-        public void OrderText(ListBox b)
+        public void OrderText(ListView b)
         {
-          //  RichTextBox box = richTextorder;
-            b.Items.Add("");
-            b.Items.Add("mom");
-            b.Items.Add("dad");
 
-            b.Items[0] = System.Drawing.Color.Green;
-            //   box.Text += $"\n order";
-            //   box.Text += $"\n order";
-            //   box.Show();
 
-        }
 
-        private void KitchenBar_Load(object sender, EventArgs e)
-        {
+
+            b.Columns.Add("order 090", b.Width - (b.Width/4));
+            b.Columns.Add("08:40", -2, System.Windows.Forms.HorizontalAlignment.Center);
+
+            //b.Columns.Add("haha", -2, HorizontalAlignment.Right);
+
+
+            // Place a check mark next to the item.
+
+
+            for (int i = 0; i < 8; i++)
+            {
+                b.Items.Add(" 4x   not chichen and not not chicken");
+                b.Items[i].Font = new Font("Arial", 14);
+                if (i == 6)
+                {
+                    b.Items.Add("");
+                    b.Items[7].SubItems.Add("Remove");
+                    break;
+                }
+            }
+
+
+
+            // Set the view to show details.
+            b.View = View.Details;
+            // Allow the user to edit item text.
+            //     b.LabelEdit = true;
+            // Allow the user to rearrange columns.
+            //   b.AllowColumnReorder = true;
+            // Display check boxes.
+
+            // Select the item and subitems when selection is made.
+            b.FullRowSelect = true;
+            // Display grid lines.
+            //  b.GridLines = true;
+            // Sort the items in the list in ascending order.
+            //      b.Sorting = SortOrder.Ascending;
+
+
+            b.Items[7].UseItemStyleForSubItems = false;
+            b.Items[7].SubItems[1].ForeColor = System.Drawing.Color.White;
+            b.Items[7].SubItems[1].BackColor = System.Drawing.Color.Black;
+            
+           
+           
             
         }
 
-        private void recall_Click(object sender, EventArgs e)
+       
+
+        private void recall_Click_1(object sender, EventArgs e)
         {
-            recallpanel.Show();
+            if (recallpanel.Visible)
+                recallpanel.Hide();
+            else
+                recallpanel.Show();
         }
 
-        private void back_Click(object sender, EventArgs e)
+        private void listViewrecall_SelectedIndexChanged(object sender, EventArgs e)
+        {
+             recallpanel.Hide();
+        }
+
+        private void Home_Click(object sender, EventArgs e)
         {
             this.Close();
             Overview.ActiveForm.Activate();
-
         }
 
-        private void recalled_Click(object sender, EventArgs e)
-        {
-            recallpanel.Hide();
-        }
+
+
+
+
+
+
+      
+
+       
+
+      
 
         
 
-        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+
+        private void KitchenBar_Load(object sender, EventArgs e) 
+        {
+            
+        }
+        private void listViewrecall_MouseHover(object sender, EventArgs e)
         {
 
+        }
+        private void PanelOrders_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            
         }
     }
 }
